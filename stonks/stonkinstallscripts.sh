384e39876bab3addfefca29569db220027a18866
Chris Paiano <chris.paiano@gmail.com>
2021-07-04 13:47:20 -0700

    Stonk update and install scripts

---
+++ stonks/stonkinstallscripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+ln --symbolic --force --verbose ~/piexperiments/stonks/stonkotrackers_web.sh ~/Desktop/stonkotrackers_web.sh
+ln --symbolic --force --verbose ~/piexperiments/stonks/stonkotrackers_web_withtoolbars.sh ~/Desktop/stonkotrackers_web_withtoolbars.sh
+ln --symbolic --force --verbose ~/piexperiments/stonks ~/Desktop/stonks
+
+sudo cp -vf ~/piexperiments/stonks/stonkupdatescripts.sh /usr/local/bin/
---
+++ stonks/stonkupdatescripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pushd ~/piexperiments
+git pull
+stonks/stonkinstallscripts.sh
+popd
