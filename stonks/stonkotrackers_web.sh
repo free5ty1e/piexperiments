11e9acbc6495cee3ed7c600645c31e19e5063162
Chris Paiano <chris.paiano@gmail.com>
2021-07-04 13:03:35 -0700

    Increased sequencing time to 45 seconds for pi 2

--- stonks/stonkotrackers_web.sh
+++ stonks/stonkotrackers_web.sh
@@ -3,6 +3,6 @@
 
 chromium-browser --app=https://www.robinhood.com/ --new-window --window-position="74,66" --window-size="1846,1008" --force-device-scale-factor=1.75 --noerrdialogs --allow-running-insecure-content --remember-cert-error-decisions &
 
-sleep 15
+sleep 45
 
 chromium-browser --app=https://coinmarketcap.com/currencies/shiba-inu/ --new-window --window-position="2,66" --window-size="1500,1200" --force-device-scale-factor=0.85 --noerrdialogs --allow-running-insecure-content --remember-cert-error-decisions --user-data-dir="/home/pi/Documents/Profiles/1" &
