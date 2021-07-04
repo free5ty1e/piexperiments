017ca564d1432b8e91f5157ee0add25ef14cfc00
Chris Paiano <chris.paiano@gmail.com>
2021-07-04 12:43:26 -0700

    now with separate profiles for each window to allow the different window sizing

--- stonks/stonkotrackers_web.sh
+++ stonks/stonkotrackers_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-chromium-browser --app=https://www.robinhood.com/ --new-window --window-position="74,66" --window-size="1846,1008" --force-device-scale-factor=1.75 --noerrdialogs --allow-running-insecure-content --remember-cert-error-decisions &
+chromium-browser --app=https://www.robinhood.com/ --new-window --window-position="74,66" --window-size="1846,1008" --force-device-scale-factor=1.75 --noerrdialogs --allow-running-insecure-content --remember-cert-error-decisions --user-data-dir="/home/pi/Documents/Profiles/1" &
 
-chromium-browser --app=https://coinmarketcap.com/currencies/shiba-inu/ --new-window --window-position="2,66" --window-size="1336,1008" --force-device-scale-factor=0.85 --noerrdialogs --allow-running-insecure-content --remember-cert-error-decisions &
+chromium-browser --app=https://coinmarketcap.com/currencies/shiba-inu/ --new-window --window-position="2,66" --window-size="1336,1008" --force-device-scale-factor=0.85 --noerrdialogs --allow-running-insecure-content --remember-cert-error-decisions --user-data-dir="/home/pi/Documents/Profiles/2" &
