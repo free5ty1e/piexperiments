cd78ed1c4e257b31a7a0d30211f51aa6a5c71ee0
Chris Paiano <chris.paiano@gmail.com>
2021-07-04 13:36:16 -0700

    Reenabled app mode for main script, created new script to launch with toolbars to allow customizing extensions and such

---
+++ stonks/stonkotrackers_web_withtoolbars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+chromium-browser https://www.robinhood.com/ --new-window --window-position="74,66" --window-size="1846,1008" --force-device-scale-factor=1.75 --noerrdialogs --allow-running-insecure-content --remember-cert-error-decisions &
+
+sleep 45
+
+chromium-browser https://coinmarketcap.com/currencies/shiba-inu/ --new-window --window-position="2,66" --window-size="1500,1200" --force-device-scale-factor=0.85 --noerrdialogs --allow-running-insecure-content --remember-cert-error-decisions --user-data-dir="/home/pi/Documents/Profiles/1" &
