19cb2cc615a165147ca5bc1ef23bd954c14514a0
Chris Paiano <chris.paiano@gmail.com>
2021-09-05 19:31:17 -0700

    Created vpngateconnect.sh script from windows from existing executable file, let's see if this creates another executable file

---
+++ vpn/vpngateconnect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Connecting to VPNGate file $1 via OpenVPN with parameters $2 $3 $4 $5"
+sudo bash -c 'openvpn --config "$1" --auth-user-pass <(echo vpn; echo vpn) "$2" "$3" "$4" "$5"'
