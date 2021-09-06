bc76c3b985326846495231350d52a5afba38c003
Chris Paiano <chris.paiano@gmail.com>
2021-09-05 22:02:28 -0700

    Fixed install script for vpngate ovpn

--- vpn/vpngateinstallovpn.sh
+++ vpn/vpngateinstallovpn.sh
@@ -2,5 +2,5 @@
 
 
 echo "This will modify and install the given .ovpn file $1 as the openvpn service's main connection"
-sed -i 's/#auth-user-pass/auth-user-pass pass/g' "$1"
+sed -i 's_#auth-user-pass_auth-user-pass /etc/openvpn/pass_g' "$1"
 sudo cp -vf "$1" /etc/openvpn/client.conf
