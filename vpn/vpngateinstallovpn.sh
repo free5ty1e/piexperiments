06578aa1f5b66c37b522885c5bff8bf864637521
Chris Paiano <chris.paiano@gmail.com>
2021-12-26 11:05:04 -0800

    now installing a vpn conf will also set keepalive 2 10 for a more stable connection

--- vpn/vpngateinstallovpn.sh
+++ vpn/vpngateinstallovpn.sh
@@ -2,5 +2,6 @@
 
 
 echo "This will modify and install the given .ovpn file $1 as the openvpn service's main connection"
-sed -i 's_#auth-user-pass_auth-user-pass /etc/openvpn/pass_g' "$1"
+cp -vf "$1" "$1.bak"
+sed -i 's_#auth-user-pass_auth-user-pass /etc/openvpn/pass_g\nkeepalive 2 10' "$1"
 sudo cp -vf "$1" /etc/openvpn/client.conf
