5d86ee65dfc46aedf2df7787981fa5d767693642
Chris Paiano <chris.paiano@gmail.com>
2021-12-26 11:09:04 -0800

    Corrected sed syntax, i think

--- vpn/vpngateinstallovpn.sh
+++ vpn/vpngateinstallovpn.sh
@@ -3,5 +3,5 @@
 
 echo "This will modify and install the given .ovpn file $1 as the openvpn service's main connection"
 cp -vf "$1" "$1.bak"
-sed -i 's_#auth-user-pass_auth-user-pass /etc/openvpn/pass_g\nkeepalive 2 10' "$1"
+sed -i 's_#auth-user-pass_auth-user-pass /etc/openvpn/pass\nkeepalive 2 10_g' "$1"
 sudo cp -vf "$1" /etc/openvpn/client.conf
