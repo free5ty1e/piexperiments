ee6962c7fd7513dc9929b95c8a4e17e7622acf51
Chris Paiano <chris.paiano@gmail.com>
2022-05-27 13:28:47 -0700

    added status text to steps in reset script

--- vpn/vpnresettransmissionvpnandbinding.sh
+++ vpn/vpnresettransmissionvpnandbinding.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+echo "Stopping Transmission..."
+sudo service transmission-daemon stop
+
+echo "Restarting OpenVPN..."
 sudo service openvpn restart
+
+echo "Waiting 45 seconds..."
 sleep 45
 
 vpnchecktunnelandbinding.sh 
 
+echo "Restarting Transmission..."
 sudo service transmission-daemon restart
 
 vpnchecktunnelandbinding.sh 
