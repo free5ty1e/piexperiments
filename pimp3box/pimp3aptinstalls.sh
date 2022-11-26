738d65c484f5302db153c1150d92a52812cc9ce8
Chris Paiano <chris.paiano@gmail.com>
2022-11-26 12:58:43 -0800

    pimp3 apt installs and pip installs

--- pimp3box/pimp3aptinstalls.sh
+++ pimp3box/pimp3aptinstalls.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-sudo apt-get -y install mpv ffmpeg
-#mpg321
+sudo apt-get -y install mpv ffmpeg libglib2.0-dev python3 python3-pip
+
+sudo apt-get -y autoremove
+
+sudo apt-get -y clean
+
+echo "Attempting pip3 installs now..."
+
+pip3 install --user mps-youtube
+pip3 install --user youtube-dl
+pip3 install --user youtube-dl --upgrade
+pip3 install --user dbus-python pygobject
+
+
+
+echo "If any errors were encountered, try the following and then run this pimp3aptinstalls.sh script again"
+
+echo "Ensure the following line is in your .bashrc :"
+echo 'export PATH="$HOME/.local/bin:$PATH"'
+
+
