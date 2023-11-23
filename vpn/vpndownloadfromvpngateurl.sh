313614b81f1691d92d11fc3aef72389822e0ae90
Chris Paiano <chris.paiano@gmail.com>
2023-11-23 12:31:23 -0800

    Safer, stopping both ovpn and transmission when refreshing

--- vpn/vpndownloadfromvpngateurl.sh
+++ vpn/vpndownloadfromvpngateurl.sh
@@ -5,10 +5,10 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-echo "Pausing openvpn service to download new vpngate.ovpn file from $1"
+echo "Stopping openvpn and transmission services to download new vpngate.ovpn file from $1"
 
+sudo service transmission-daemon stop
 sudo service openvpn stop
 wget --output-document="ovpn/vpngate.ovpn" "$1"
-sudo service openvpn start
 
 echo "Download completed!"
